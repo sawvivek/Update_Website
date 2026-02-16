df97b949460ff2c76e11b1d7671c96018194f873
vivekdemo <viveksaw144@gmail.com>
2026-02-16 20:23:00 +0530

    Initial commit

--- src/assets/about/index.js
+++ src/assets/about/index.js
@@ -1,11 +1,11 @@
 const AboutImages = {
-    council2024: require("./team_2024.webp"),
-    Team2024: require("./team_2024.webp"),
-    Descipline: require("./Descipline.webp"),
-    Magazine2022: require("./Magazine2022.webp"),
-    Magazine2023: require("./Magazine2023.webp"),
-    Magazine2024: require("./Magazine2024.webp"),
-    Punctuality: require("./Punctuality.webp"),
-    Respect: require("./Respect.webp")
-}
-export default AboutImages
+  council2024: require("./team_2025.jpeg"),
+  Team2024: require("./team_2024.webp"),
+  Descipline: require("./Descipline.webp"),
+  Magazine2022: require("./Magazine2022.webp"),
+  Magazine2023: require("./Magazine2023.webp"),
+  Magazine2024: require("./Magazine2024.webp"),
+  Punctuality: require("./Punctuality.webp"),
+  Respect: require("./Respect.webp"),
+};
+export default AboutImages;
